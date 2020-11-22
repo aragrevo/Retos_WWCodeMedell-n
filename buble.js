c255a50b860de7ff635a5e96383616b1bfac6e80
aragrevo <eduver_san@hotmail.com>
2020-11-21 19:27:04 -0500

    Bubble sort

--- buble.js
+++ buble.js
@@ -2,10 +2,33 @@
  * @param {*} array array of integers
  */
 const bubleSort = (array) => {
-    let arraySort = array
-    // Write your code here
+  let arraySort = [...array];
+  const lng = arraySort.length;
+  let stop = false;
+  for (let i = 0; i < lng; i++) {
+    stop = false;
+    for (let j = 0; j < lng - i; j++) {
+      const current = arraySort[j];
+      const adjacent = arraySort[j + 1];
+      if (current > adjacent) {
+        [arraySort[j], arraySort[j + 1]] = [adjacent, current];
+        stop = true;
+      }
+    }
+    if (!stop) {
+      break;
+    }
+  }
+  return arraySort;
+};
 
-    return arraySort
-}
+const buildArray = (qty) => {
+  let array = [];
+  for (let index = 0; index < qty; index++) {
+    array.push(Math.floor(Math.random() * 100));
+  }
+  return array;
+};
 
-console.log(bubleSort(array))
+const array = buildArray(20);
+console.log(bubleSort(array));
